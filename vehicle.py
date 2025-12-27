ed881681227e8c75adb84a2b4cb0c827a58a0503
ashwinipatil1281@gmail.com <ashwinipatil1281-dev>
2025-12-27 12:43:35 +0530

    save

--- vehicle.py
+++ vehicle.py
@@ -1,12 +1,13 @@
-def vehicle_details(vId,vname.price,yop):
-    result=(
+def vehicle_details(vId, vname, price, yop):
+    result = (
         f"vId: {vId}\n"
         f"vname: {vname}\n"
         f"Price: {price}\n"
-        f"YOP: {yop}\n"
+        f"YOP: {yop}"
     )
     return result
 
-if __name__ == "__main__":
- print(vehicle_details(103 , BMW, 10000, 2025))
 
+if __name__ == "__main__":
+   
+    print(vehicle_details("P102", "BMW", 100000, 2025))
